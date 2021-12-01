204ac0f89bab88960fc7a424102dc50e716e85c7
Bozo Dragojevic <bozzo@digiverse.si>
2021-12-01 11:40:34 +0100

    Wed Dec  1 11:40:34 CET 2021

---
+++ x.py
@@ -0,0 +1,35 @@
+import functools
+import itertools
+import re
+import collections
+import pprint
+import operator
+import time
+from dataclasses import dataclass
+import numpy as np
+from pprint import pprint
+
+def fst(x):
+    return x[0]
+
+def snd(x):
+    return x[1]
+
+def dbg(*x, **y):
+    # print(*x, **y)
+    pass
+
+def test():
+    pass
+
+
+test()
+
+for name in [("test_input"),
+             ("input")][:1]:
+    print("=======\n",name, flush=True)
+    with open(name) as f:
+        a = tuple(int(l.strip()) for l in f)
+        print(a)
+
+print("==================",flush=True)

cc3f27c3e72c727bec8fb31df4f3cc142121c152
Bozo Dragojevic <bozzo@digiverse.si>
2021-12-04 08:25:02 +0100

    Sat Dec  4 08:25:02 CET 2021

--- 2021/4/x.py
+++ 2021/4/x.py
@@ -115,7 +115,7 @@ def parse(f):
     return Bingo(rand=rand, boards=boards)
 
 for name in [("test_input"),
-             ("input")][:1
+             ("input")][:2
                         ]:
     print("=======\n",name, flush=True)
     with open(name) as f:

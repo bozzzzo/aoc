1023f971bf7d1b2887558001e63754edf78ce56d
Bozo Dragojevic <bozzo@digiverse.si>
2021-12-04 07:32:40 +0100

    Sat Dec  4 07:32:40 CET 2021

---
+++ 2021/4/x.py
@@ -0,0 +1,79 @@
+import functools
+import itertools
+import re
+import collections
+import pprint
+import operator
+import time
+from dataclasses import dataclass
+import numpy as np
+from typing import *
+from pprint import pprint
+
+def fst(x):
+    return x[0]
+
+def snd(x):
+    return x[1]
+
+def dbg(*x, **y):
+    # print(*x, **y)
+    pass
+
+
+def first(a):
+    pass
+
+def second(a):
+    pass
+
+def test():
+    pass
+
+
+test()
+
+def strint(x):
+    try:
+        return int(x)
+    except:
+        pass
+    return x
+
+@dataclasses.dataclass
+class Board:
+    rows: Tuple[int]
+
+
+@dataclasses.dataclass
+class Bingo:
+    rand: List[str]
+    boards: List[Board]
+
+def parse(f):
+    rand = list(map(int, f.readline().strip().split(',')))
+    def parse_boards(f):
+        while True:
+            try:
+                assert not f.readline().strip()
+            except:
+                raise StopIteration
+            rows = []
+            for _ in range(5):
+                rows.append(tuple(map(int, f.readline().split())))
+            yield Board(rows=rows)
+    boards = list(parse_boards(f))
+    return b
+
+for name in [("test_input"),
+             ("input")][:2
+                        ]:
+    print("=======\n",name, flush=True)
+    with open(name) as f:
+        a = parse(f)
+    print(a)
+
+    first(a)
+    #second(a)
+
+print("==================",flush=True)

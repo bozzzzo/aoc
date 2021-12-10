6dc80d81c4d15100e3f62fb12f8935eaa65cfc51
Bozo Dragojevic <bozzo@digiverse.si>
2021-12-10 06:04:14 +0100

    .

---
+++ 2021/10/x.py
@@ -0,0 +1,68 @@
+import functools
+import itertools
+import re
+import collections
+import pprint
+import operator
+import time
+from dataclasses import dataclass, replace, field
+import numpy as np
+from typing import *
+from pprint import pprint
+
+def fst(x):
+    return x[0]
+
+def snd(x):
+    return x[1]
+
+def dbg(*x, **y):
+    # print(*x, **y)
+    pass
+
+def irange(a,b):
+    d = 1 if b >= a else -1
+    return range(a,b+d,d)
+
+
+
+def first(a):
+    pass
+
+def second(a):
+    pass
+
+def test():
+    pass
+
+
+test()
+
+def strint(x):
+    try:
+        return int(x)
+    except:
+        pass
+    return x
+
+
+def parse(f):
+    def parse_line(l):
+        return tuple(map(int,l.strip()))
+    return {(x,y):z for y,row in enumerate(map(parse_line, f)) for x,z in enumerate(row)}
+    pass
+
+for name in [("test_input"),
+             ("input")][:2
+                        ]:
+    print("=======\n",name, flush=True)
+    with open(name) as f:
+        a = parse(f)
+    print(a)
+
+    w = first(a)
+    print("first", w)
+    w = second(a)
+    print("second", w)
+
+print("==================",flush=True)

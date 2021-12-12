73ac8fc898b390cae8ffff12e6113b76bdce299b
Bozo Dragojevic <bozzo@digiverse.si>
2021-12-12 07:07:06 +0100

    Sun Dec 12 07:07:06 CET 2021

--- 2021/12/x.py
+++ 2021/12/x.py
@@ -30,7 +30,7 @@ def interesting1(a, big, path, next):
 
 def interesting2(a, big, path, next):
     c = collections.Counter(path + (next,))
-    x = [k for k,v in c.items() if k not in big and v > 1]
+    x = [k,v for k,v in c.items() if k not in big and v > 1]
     print(path, next, c, x)
     return len(x) < 2
 

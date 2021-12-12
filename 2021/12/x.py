bd7512847e089341d1d87469b02eb79167567d35
Bozo Dragojevic <bozzo@digiverse.si>
2021-12-12 06:20:37 +0100

    Sun Dec 12 06:20:37 CET 2021

--- 2021/12/x.py
+++ 2021/12/x.py
@@ -63,7 +63,7 @@ def parse(f):
     flinks = tuple(map(parse_line, f))
     rlinks = tuple((b,a) for a,b in flinks)
     links = sorted((a,b) for a,b in flinks+rlinks if b not in ('start', 'end'))
-    return dict()
+    return dict(links)
     pass
 
 for name in [("test_input"),

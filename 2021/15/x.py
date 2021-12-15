ca07df9fe01d6938768b3f965269ff433ccd8e4c
Bozo Dragojevic <bozzo@digiverse.si>
2021-12-15 06:35:20 +0100

    Wed Dec 15 06:35:20 CET 2021

--- 2021/15/x.py
+++ 2021/15/x.py
@@ -25,8 +25,29 @@ def irange(a,b):
     return range(a,b+d,d)
 
 
+def find_path(a):
+    costs = {(0,0): (0,())} # coord : (cost, path)
+    infinite = (99999999999999, ())
+    active = [(0,0)]
+    while active:
+        active, current_active = [], active
+        for current in current_active:
+            base_risk, base_path = costs[current]
+            for dx, dy in ((-1, 0), (0, -1), (1, 0), (0, 1)):
+                coord = (path[-1][0] + dx, path[-1][1] + dy)
+                if coord not in a:
+                    continue
+                new_risk = base_risk + a[coord]
+                current_risk, current_path = costs.get(coord, infinite)
+                if new_risk < current_risk:
+                    costs[coord] = (new_risk, current_path + (coord, ))
+                    active.append(coord)
+
+    return costs(max(costs))
+
 
 def first(a):
+    return find_path(a)
     pass
 
 def second(a):

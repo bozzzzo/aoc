064048e7887aead72b3f0f140c87e1c04e732c7f
Bozo Dragojevic <bozzo@digiverse.si>
2021-12-15 06:04:10 +0100

    Wed Dec 15 06:04:10 CET 2021

--- 2021/15/x.py
+++ 2021/15/x.py
@@ -78,7 +78,7 @@ for name in [("test_input"),
                         ]:
     print("=======\n",name, flush=True)
     with open(name) as f:
-        a = parse(f)
+        a = parse_grid(f)
     print(a)
 
     w = first(a)

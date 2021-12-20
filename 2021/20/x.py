dbc585e6552d95606b2f830bc6291c2b3df9da7b
Bozo Dragojevic <bozzo@digiverse.si>
2021-12-20 06:16:47 +0100

    Mon Dec 20 06:16:47 CET 2021

--- 2021/20/x.py
+++ 2021/20/x.py
@@ -102,7 +102,7 @@ for name in [("test_input"),
     print("=======\n",name, flush=True)
     with open(name) as f:
         a = parse(f)
-    # pprint(a)
+    # pprint(a) 
 
     w = first(a)
     print("first", w) 

b60743879fb9b4eb386d41ee574576d93b341686
Bozo Dragojevic <bozzo@digiverse.si>
2021-12-23 07:15:53 +0100

    Thu Dec 23 07:15:53 CET 2021

--- 2021/23/x.py
+++ 2021/23/x.py
@@ -31,7 +31,7 @@ def irange(a,b):
 def srange(a,b):
     return irange(a,b) if a < b else irange(b,a)
 
-targets = [(c, (x*2 + 1, y)) for x, c in enumerate('abcd', 1) for y in (2,3)]
+targets = [((x*2 + 1, y), c) for x, c in enumerate('abcd', 1) for y in (2,3)]
 
 def first(a):
     for k,v in targets:

7a36abcd11cfd3eb01fe8fbc6beda9b830931fb0
Bozo Dragojevic <bozzo@digiverse.si>
2021-12-25 06:43:03 +0100

    Sat Dec 25 06:43:03 CET 2021

--- 2021/25/x.py
+++ 2021/25/x.py
@@ -147,13 +147,13 @@ def parse(f):
 for name in [("test_input"),
              #("test_input2"),
              #("test_input3"),
-             ("input")][:1
+             ("input")][:2
                         ]:
     print("=======\n",name, flush=True)
     with open(name) as f:
         f = f.read()
     a = parse_grid(f.splitlines())
-    show_grid(a)
+    #show_grid(a)
 
     w = first(a)
     print("first", w)
